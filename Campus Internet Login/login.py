84a33b308264d1d9b74dd07e74055f84a45ea055
laveesingh <laveechaudharytheone@gmail.com>
2016-02-17 03:29:06 +0530

    something

--- Campus Internet Login/login.py
+++ Campus Internet Login/login.py
@@ -23,7 +23,7 @@ uelement.send_keys("14bcs027")  # Write your entry number instead of 14bcs027
 time.sleep(1)
 pelement.click()
 time.sleep(1)
-pelement.send_keys("L4vee0n1yiAB") # Write your password here instead of mine
+pelement.send_keys("password") # Write your password here instead of mine
 time.sleep(1)
 lelement.click()
 time.sleep(3)
