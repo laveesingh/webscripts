fcbddef6b2fbcb1582be3c83826d9b8025ab06c3
lavee singh <laveechaudharytheone@gmail.com>
2016-02-20 16:25:34 +0530

    login_win.py
    Login script for windows added

---
+++ Campus Internet Login/login_win.py
@@ -0,0 +1,33 @@
+#!C:\Python\python.exe
+
+from selenium import webdriver
+import time
+from pyvirtualdisplay import Display
+
+#display = Display(visible=0, size=(800, 500))
+#display.start()
+
+browser = webdriver.Chrome("C:\Python27\Scripts\chromedriver.exe")
+time.sleep(1)
+url = "http://172.16.166.10"
+
+browser.get(url)
+time.sleep(3)
+uelement = browser.find_element_by_css_selector(".textfield2>input[type='text']")
+pelement = browser.find_element_by_css_selector(".textfield2>input[type='password']")
+lelement = browser.find_element_by_css_selector(".login2>input[type='image']")
+
+uelement.click()
+time.sleep(1)
+uelement.send_keys("14bcs027")  # Write your entry number instead of 14bcs027
+time.sleep(1)
+pelement.click()
+time.sleep(1)
+pelement.send_keys("L4vee0n1yiAB") # Write your password here instead of mine
+time.sleep(1)
+lelement.click()
+time.sleep(3)
+
+browser.close() # Remove this line, if you don't want browser to get closed after login process.
+
+#display.stop()
