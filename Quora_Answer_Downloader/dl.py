3cd2a2ac7d386b7e8267207fe924463a7b656c89
arjunsinghy96 <arjunsinghy96@gmail.com>
2016-10-26 22:31:46 +0530

    Added ImportError check and print_function

--- Quora_Answer_Downloader/dl.py
+++ Quora_Answer_Downloader/dl.py
@@ -1,14 +1,25 @@
+from __future__ import print_function
 
-from selenium import webdriver
+from sys import exit
+try:
+	from selenium import webdriver
+	from selenium.webdriver.common.keys import Keys
+	from pyvirtualdisplay import Display 
+	import pdfkit
+except ImportError as ie:
+	print("\n... Dependencies not met :")
+	print("...","ImportError: ", ie)
+	print("\n... Please run [ sudo sh dependencies.sh ]\n")
+	exit(-1)
+
+
 import time
-from selenium.webdriver.common.keys import Keys
-from pyvirtualdisplay import Display 
-import pdfkit
 import os
 import re
 import subprocess as sp
 
 url = raw_input("Please enter the url of the webpage: ");
+# assert isinstance(url,str)
 display = Display(visible=0, size=(1024, 768))
 display.start()
 browser = webdriver.Chrome()
@@ -20,25 +31,25 @@ time.sleep(3)
 body = browser.find_element_by_tag_name("body")
 t = 25
 
-print "Loading all the answers...."
+print("Loading all the answers....")
 sp.call(["notify-send", "Attention", "Lazy loading of answers will take a minute or two, please be patient. File will be saved as answer.pdf in current directory."])
 while t > 0:
 	body.send_keys(Keys.END)
 	time.sleep(3)
 	t -= 1
-print "All the answers loaded."
-print "Getting the html source."
+print("All the answers loaded.")
+print("Getting the html source.")
 html_source = browser.page_source
-print "HTML fetched."
-print "now creating pdf file..."
+print( "HTML fetched.")
+print ("now creating pdf file...")
 
 pat = ".*www.quora.com/(.*)"
 fname = re.findall(pat, url)[0]
 fname = fname + '.pdf'
 
 pdfkit.from_string(html_source, fname)
-print "File will be saved as " + fname + " in current directory."
-print "File created."
+print("File will be saved as " + fname + " in current directory.")
+print("File created.")
 browser.close()
 display.stop()
 os.system("xdg-open "+fname)
